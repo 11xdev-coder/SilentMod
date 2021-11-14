64fe09aa1be2e32b5ff3f4368f937761ac33c41d
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2021-11-14 22:02:32 +0300

    Added aglet craft

--- SilentMod.cs
+++ SilentMod.cs
@@ -1,8 +1,19 @@
 using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
 
 namespace SilentMod
 {
 	public class SilentMod : Mod
 	{
-	}
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.IronBar, 5);
+            recipe.anyIronBar = true;
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(ItemID.Aglet);
+            recipe.AddRecipe();
+        }
+    }
 }
